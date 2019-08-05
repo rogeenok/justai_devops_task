935a3606671ccdc1add074aa5b5026662a84f8d6
Igor Konovalov <rogee.nok@gmail.com>
2019-08-05 17:51:10 +0300

    little fix to script

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ git fetch
 difff=$(git diff @{upstream})
 len=${#difff}
 
-if (( $len == 0 )) ; then
+if (( $len >= 0 )) ; then
 	# making a docker image
 	docker build -t devops-app . --label "maintainer=$(git log -1 --pretty=format:'%an')" \
 		--label "branch=$(git branch | grep \* | cut -d ' ' -f2)" \
@@ -13,5 +13,5 @@ if (( $len == 0 )) ; then
 	# stop running container
 	docker stop dapp-container && docker rm dapp-container
 	# run new one
-	docker run -d --name dapp-container -p 127.0.0.1:80:8080/tcp devops-app
+	docker run -d --name dapp-container -p 80:8080/tcp devops-app
 fi
