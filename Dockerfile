e5a7d91352c5296c6b5e1beedf96259601ec3879
rogeeNok <rogee.nok@gmail.com>
2019-08-05 19:41:20 +0300

    tagging added

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@
 FROM python:rc-alpine
 COPY src ./home/devops-task/src
 
+WORKDIR ./home/devops-task/src
+
 # -- run python server (inner port 8080)
 ENTRYPOINT ["python", "/home/devops-task/src/server.py"]
 EXPOSE 8080
--- script.sh
+++ script.sh
@@ -1,17 +1,51 @@
 #!/bin/bash
 
-# checking the difference in branches (currently just master)
-git fetch
-difff=$(git diff @{upstream})
-len=${#difff}
 
-if (( $len >= 0 )) ; then
-	# making a docker image
-	docker build -t devops-app . --label "maintainer=$(git log -1 --pretty=format:'%an')" \
-		--label "branch=$(git branch | grep \* | cut -d ' ' -f2)" \
-		--label "commit=$(git log -1 --pretty=format:'%H')"
+# update remote branches refs
+git remote update
+
+current=$(git branch | grep \* | cut -d ' ' -f2)
+echo "current branch is : $current"
+
+status=$(git status | grep -E "behind" | cut -d ' ' -f 4)
+len=${#status}
+
+# echo $status $len
+
+if (( $len == 0 )) ; then
+	# pull changes
+	git pull
+	
+	#check the existence of image tagged with current branch
+	tag=$(docker images | grep devops-app | tr -s ' ' | cut -d ' ' -f 2 | grep $current)
+	echo $tag
+	
+	oldtag=$tag
+	o_version="0.1"
+	n_version=$o_version
+	
+	# update tag version if the prior exists
+	if (( ${#tag} != 0 )) ; then
+		o_version=$(echo $tag | cut -d 'v' -f 2)
+		echo $o_version
+		# update the version for tag
+	else
+		# create a new tag (v0.1)
+		tag="$(echo $current | cut -d '=' -f 2)-v$n_version"
+		echo "new tag $tag is set for branch $current"
+	fi
+	
+	# save metadata	
+	maintainer="maintainer=$(git log -1 --pretty=format:'%an')"
+	branch="branch=$current"
+	commit="commit=$(git log -1 --pretty=format:'%H')"
+	
+	# build a docker image
+	docker build -t devops-app:$tag . --label "$maintainer" \
+		--label "$branch" \
+		--label "$commit"
 	# stop running container
-	docker stop dapp-container && docker rm dapp-container
+	docker stop dappc-$oldtag && docker rm dappc-$oldtag
 	# run new one
-	docker run -d --name dapp-container -p 80:8080/tcp devops-app
+	docker run -d --name dappc-$tag -p 80:8080/tcp devops-app:$tag
 fi
